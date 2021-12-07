4e94eb1c69ff1817f75ac56e83b76b5c6f62e0e4
LegyenKristof <48032365+LegyenKristof@users.noreply.github.com>
2021-12-07 23:59:23 +0100

    Teljesen kész

--- src/main/java/hu/petrik/masodfoku/HelloController.java
+++ src/main/java/hu/petrik/masodfoku/HelloController.java
@@ -46,10 +46,10 @@ public class HelloController {
             double x1 = ((-1 * dB) + Math.sqrt(diszkriminans)) / (2 * dA);
             double x2 = ((-1 * dB) - Math.sqrt(diszkriminans)) / (2 * dA);
             if(x1 == x2){
-                eredmeny.setText(String.format("x1 = x2 = %.3f", Math.round(x1*1000)/1000));
+                eredmeny.setText(String.format("x1 = x2 = %.3f", x1));
             }
             else{
-                eredmeny.setText(String.format("x1 = %.3f\nx2 = %.3f", Math.round(x1*1000)/1000, Math.round(x2*1000)/1000));
+                eredmeny.setText(String.format("x1 = %.3f\nx2 = %.3f", x1, x2));
             }
         }
     }
