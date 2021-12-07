9a9bef063eaa2126b7f9cbf2983944409e4c8410
LegyenKristof <48032365+LegyenKristof@users.noreply.github.com>
2021-12-07 23:34:24 +0100

    Validálás

--- src/main/java/hu/petrik/masodfoku/HelloController.java
+++ src/main/java/hu/petrik/masodfoku/HelloController.java
@@ -2,13 +2,39 @@ package hu.petrik.masodfoku;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextField a;
+    @FXML
+    public TextField b;
+    @FXML
+    public TextField c;
+    @FXML
+    public Label eredmeny;
+
+    @FXML
+    public void szamol(MouseEvent mouseEvent) {
+        if(a.getText() != "" && b.getText() != "" && c.getText() != ""){
+            try{
+                Double.parseDouble(a.getText());
+                Double.parseDouble(b.getText());
+                Double.parseDouble(c.getText());
+                kiszamol();
+            }
+            catch (Exception e){
+                eredmeny.setText("A mezőkbe valós\nszámokat kell megadnia");
+            }
+        }
+        else{
+            eredmeny.setText("Minden mező\nkitöltése kötelező");
+        }
+    }
+
+    private void kiszamol(){
+
     }
 }
